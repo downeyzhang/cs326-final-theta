8d36411354581c9e892808b4b13bc21de916315e
Donglin Zhang <dlinkzhang@yahoo.com>
2020-05-04 10:28:07 -0400

    change database url

--- src/db.js
+++ src/db.js
@@ -1,3 +1,6 @@
 var mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost:27017/mydb");
+
+//database name: theta_database
+//collection: user, post
+mongoose.connect("mongodb+srv://admin:admin@thetadatabase-peevn.mongodb.net/test?retryWrites=true&w=majority");
 module.exports = mongoose;
