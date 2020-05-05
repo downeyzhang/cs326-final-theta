b1402b227b24d3448ee08c4866a97fe90a234f43
jiamingyuan <43215425+jiamingyuan@users.noreply.github.com>
2020-05-04 23:42:11 -0400

    Update db.js

--- src/db.js
+++ src/db.js
@@ -1,6 +1,6 @@
 var mongoose = require("mongoose");
 // const uri = "mongodb://localhost:27017/mydb";
-const uri = "mongodb+srv://Jiamingyuan:yjm19990601@teammaker-cluster-w06oc.mongodb.net/teammaker?retryWrites=true&w=majority"
+const uri = "mongodb+srv://admin:esQBUyKRlHvkh1pM@teammaker-cluster-w06oc.mongodb.net/teammaker?retryWrites=true&w=majority"
 mongoose.connect(uri, function(err) {
     if (err) {
         console.log("MongoDB is not connected!");
